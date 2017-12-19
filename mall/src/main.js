2b9cb27f04e8f512d5f099ced227c6e999a2f39e
zhuran <529865274@qq.com>
2017-12-19 11:19:45 +0800

    添加切图

--- mall/src/main.js
+++ mall/src/main.js
@@ -4,12 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import store from './store'
+
+
+console.log(store)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
