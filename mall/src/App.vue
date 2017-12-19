b19f93409697f8a49002b7477a62371f27c61a47
zhuran <529865274@qq.com>
2017-12-19 11:19:27 +0800

    添加切图

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  	<header-nav></header-nav>	
+  	<router-view></router-view>
   </div>
 </template>
 
 <script>
+
+import headerNav from '@/components/header-nav'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    headerNav
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
