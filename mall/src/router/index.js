c46edc176c0779b2ed1be8b9655b493043030640
zhuran <529865274@qq.com>
2017-12-19 14:25:52 +0800

    init

---
+++ mall/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import '@/assets/css/reset.css'
+import '@/assets/css/header.css'
+
+Vue.use(Router)
+
+import Shop from '@/views/shop'
+export default new Router({
+  routes: [
+	  {
+	  	path: '/',
+	  	name: Shop,
+	  	component: Shop
+	  }
+  ]
+})
