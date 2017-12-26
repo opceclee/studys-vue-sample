b642f536ab74288f50b39d1685bb3a113f3a8589
zhuran <529865274@qq.com>
2017-12-26 11:22:56 +0800

    新增子模块

---
+++ mall/src/components/shop-item.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="item">
+		<div>
+			<div class="item-img"><img :alt="item.name" :src="item.sku_info[itemIndex].ali_image+'?x-oss-process=image/resize,w_206/quality,Q_80/format,webp'" style="opacity: 1;">
+			</div>
+			<h6>{{item.name}}</h6>
+			<h3 >{{item.name_title}}</h3>
+			<div class="params-colors">
+				<ul class="colors-list">
+					<li v-for="sku,index in item.sku_info">
+						<a href="javascript:;" @click="tabIndex(index)" :title="sku.spec_json.show_name" :class="{'active': index==itemIndex}">
+							<img :src="'http://img01.smartisanos.cn/'+ sku.spec_json.image +'/20X20.jpg'">
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="item-btns clearfix">
+				<span class="item-gray-btn"><a href="javascript:;" target="_blank">查看详情</a> </span><span class="item-blue-btn">加入购物车 </span>
+			</div>
+			<div class="item-price clearfix">
+				<i>¥</i><span>{{item.price}}</span>
+			</div>
+			<div class="discount-icon">false</div>
+			<div class="item-cover">
+				<a href="javascript:;" target="_blank"></a>
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<script>
+	/*
+	 * 默认商品的颜色索引 ：itemIndex = 0
+	 * 
+	 * */
+	export default {
+		data () {
+			return{
+				itemIndex: 0
+			}
+		},
+		props: {
+			item: {
+				type: Object
+			}
+		},
+		methods: {
+			tabIndex (index){// 选中颜色的切换效果
+				this.itemIndex = index
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
