50a38e4b9cd8b95285e85d982893a19237db025a
zhuran <529865274@qq.com>
2017-12-26 11:06:35 +0800

    更新

--- mall/src/views/shop.vue
+++ mall/src/views/shop.vue
@@ -11,34 +11,7 @@
 				</div>
 				<div class="gray-box">
 					<div class="item-box">
-						<div class="item" v-for="item,index in goodsData">
-							<div>
-								<div class="item-img"><img :alt="item.name" :src="item.sku_info[0].ali_image+'?x-oss-process=image/resize,w_206/quality,Q_80/format,webp'" style="opacity: 1;">
-								</div>
-								<h6>{{item.name}}</h6>
-								<h3 >{{item.name_title}}</h3>
-								<div class="params-colors">
-									<ul class="colors-list">
-										<li v-for="sku,index in item.sku_info">
-											<a href="javascript:;" :title="sku.spec_json.show_name">
-												<img :src="'http://img01.smartisanos.cn/'+ sku.spec_json.image +'/20X20.jpg'">
-											</a>
-										</li>
-									</ul>
-								</div>
-								<div class="item-btns clearfix">
-									<span class="item-gray-btn"><a href="javascript:;" target="_blank">查看详情</a> </span><span class="item-blue-btn">加入购物车 </span>
-								</div>
-								<div class="item-price clearfix">
-									<i>¥</i><span>199.00</span>
-								</div>
-								<div class="discount-icon">false</div>
-								<div class="item-cover">
-									<a href="javascript:;" target="_blank"></a>
-								</div>
-							</div>
-						</div>
-
+						<shop-item v-for="item,index in goodsData" :item="item"></shop-item>
 					</div>
 				</div>
 			</div>
@@ -47,11 +20,15 @@
 
 <script>
 	import goodsData from '@/lib/newGoodsData'
+	import shopItem from '@/components/shop-item'
 	export default {
 		data () {
 			return {
 				goodsData: goodsData
 			}
+		},
+		components: {
+			shopItem
 		}
 	}
 </script>
