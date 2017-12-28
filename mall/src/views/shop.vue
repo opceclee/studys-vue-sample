9cd06922490de8a507ffdd32c13898bf4079dd05
zhuran <529865274@qq.com>
2017-12-28 15:52:09 +0800

    最大值的限制弹窗

--- mall/src/views/shop.vue
+++ mall/src/views/shop.vue
@@ -1,26 +1,28 @@
 <template>
 	<div id="main">
-			<div class="sku-box store-content">
-				<div class="sort-option">
-					<ul class="line clear">
-						<li><a href="javascript:;" class="active">综合排序</a></li>
-						<li><a href="javascript:;" class="">销量排序</a></li>
-						<li><a href="javascript:;" class="">价格低到高</a></li>
-						<li><a href="javascript:;" class="">价格高到低</a></li>
-					</ul>
-				</div>
-				<div class="gray-box">
-					<div class="item-box">
-						<shop-item v-for="item,index in goodsData" :item="item"></shop-item>
-					</div>
+		<div class="sku-box store-content">
+			<div class="sort-option">
+				<ul class="line clear">
+					<li><a href="javascript:;" class="active">综合排序</a></li>
+					<li><a href="javascript:;" class="">销量排序</a></li>
+					<li><a href="javascript:;" class="">价格低到高</a></li>
+					<li><a href="javascript:;" class="">价格高到低</a></li>
+				</ul>
+			</div>
+			<div class="gray-box">
+				<div class="item-box">
+					<shop-item v-for="item,index in goodsData" :item="item"></shop-item>
 				</div>
 			</div>
 		</div>
+		<prompt></prompt>
+	</div>
 </template>
 
 <script>
 	import goodsData from '@/lib/newGoodsData'
 	import shopItem from '@/components/shop-item'
+	import prompt from '@/components/prompt'
 	export default {
 		data () {
 			return {
@@ -28,7 +30,8 @@
 			}
 		},
 		components: {
-			shopItem
+			shopItem,
+			prompt
 		}
 	}
 </script>
