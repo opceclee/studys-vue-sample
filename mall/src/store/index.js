51f7918906b97912455c94e2d39ab0f2b6087b1d
zhuran <529865274@qq.com>
2018-02-05 16:52:42 +0800

    收货地址

--- mall/src/store/index.js
+++ mall/src/store/index.js
@@ -6,7 +6,33 @@ Vue.use(Vuex)
 let store = new Vuex.Store({
 	state: {
 		carPanelData: [],
-		receiving: [],
+		receiveInfo: [{
+	      'name': '王某某',
+	      'phone': '13811111111',
+	      'areaCode': '010',
+	      'landLine': '64627856',
+	      'provinceId': 110000,
+	      'province': '北京市',
+	      'cityId': 110100,
+	      'city': '市辖区',
+	      'countyId': 110106,
+	      'county': '海淀区',
+	      'add': '上地十街辉煌国际西6号楼319室',
+	      'default': true
+	    },{
+	      'name': '李某某',
+	      'phone': '13811111111',
+	      'areaCode': '010',
+	      'landLine': '64627856',
+	      'provinceId': 110000,
+	      'province': '北京市',
+	      'cityId': 110100,
+	      'city': '市辖区',
+	      'countyId': 110106,
+	      'county': '海淀区',
+	      'add': '上地十街辉煌国际东6号楼350室',
+	      'default': false
+	    }],
 		maxOff: false,
 		carShow: false,
 		carTime: null,
